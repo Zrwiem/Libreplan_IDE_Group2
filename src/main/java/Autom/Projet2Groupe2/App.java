a92d36b11d3013ea36ac2799cd5837ea431e259c
Zrwiem <zouari.wiem@gmail.com>
2021-12-09 16:50:23 +0100

    first commit

---
+++ src/main/java/Autom/Projet2Groupe2/App.java
@@ -0,0 +1,13 @@
+package Autom.Projet2Groupe2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
